6e92520736ee1fd28c3a97269820b205e97ebe32
Abdul <hafizhahm123@gmail.com>
2024-10-31 06:06:28 +0700

    reconstruct

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -1,15 +1,18 @@
 class FavoriteMovieSearchPresenter {
   constructor() {
+    this._listenToSearchRequestByUser();
+  }
+ 
+  _listenToSearchRequestByUser() {
     this._queryElement = document.getElementById('query');
     this._queryElement.addEventListener('change', (event) => {
-      console.log(event);
       this._latestQuery = event.target.value;
     });
   }
-
+ 
   get latestQuery() {
     return this._latestQuery;
   }
 }
-
+ 
 export default FavoriteMovieSearchPresenter;
