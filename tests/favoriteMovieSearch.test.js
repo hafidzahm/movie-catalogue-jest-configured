312c4abca27cdcd971a74a0b52226d3af7565861
Abdul <hafizhahm123@gmail.com>
2024-10-31 05:51:48 +0700

    TDD init (test passed)

---
+++ tests/favoriteMovieSearch.test.js
@@ -0,0 +1,23 @@
+import FavoriteMovieSearchPresenter from '../src/scripts/views/pages/liked-movies/favorite-movie-search-presenter';
+
+describe('Searching movies', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+          <div id="movie-search-container">
+            <input id="query" type="text">
+            <div class="movie-result-container">
+              <ul class="movies">
+              </ul>
+            </div>
+          </div>
+        `;
+  });
+
+  it('should be able to capture the query typed by the user', () => {
+    const presenter = new FavoriteMovieSearchPresenter();
+    const queryElement = document.getElementById('query');
+    queryElement.value = 'film a';
+    queryElement.dispatchEvent(new Event('change'));
+    expect(presenter.userQuery).toEqual('film a');
+  });
+});
