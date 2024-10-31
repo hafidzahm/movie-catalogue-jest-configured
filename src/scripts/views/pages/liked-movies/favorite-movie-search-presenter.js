175b053259d91f08086fa739428823b91ea771c9
Abdul <hafizhahm123@gmail.com>
2024-10-31 08:31:06 +0700

    refactor kode

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -7,9 +7,13 @@ class FavoriteMovieSearchPresenter {
   _listenToSearchRequestByUser() {
     this._queryElement = document.getElementById('query');
     this._queryElement.addEventListener('change', (event) => {
-      this._latestQuery = event.target.value;
-      this._favoriteMovies.searchMovies(this._latestQuery);
+      this._searchMovies(event.target.value);
     });
+  }
+
+  _searchMovies(latestQuery) {
+    this._latestQuery = latestQuery;
+    this._favoriteMovies.searchMovies(this.latestQuery);
   }
 
   get latestQuery() {
